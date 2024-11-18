31825e57eb6e053dbdd498891fd0b68f5f7500c9
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-11-18 22:55:42 +0200

    Update App.jsx

--- meteor-wallet/imports/ui/App.jsx
+++ meteor-wallet/imports/ui/App.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 export const App = () => (
   <div>
     <h1>Meteor Wallet</h1>
+    <form>
+      <label for="name">
+        Name
+      </label>
+      <input type="text"></input>
+    </form>
   </div>
+  
 );
