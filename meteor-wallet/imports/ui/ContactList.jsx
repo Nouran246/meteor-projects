a84ae3efd277714629ce1cb863f2e7dca16956da
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-11-22 22:40:58 +0200

    Update ContactList.jsx

--- meteor-wallet/imports/ui/ContactList.jsx
+++ meteor-wallet/imports/ui/ContactList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { ContactsCollection } from "../api/ContactsCollections";
 export const ContactList = () => {
+    const contacts= ContactsCollection.find({}).fetch(); //Tracker is a tracking system in meteor
     return(
 <>
 <h3>Contact List</h3>
+{contacts.map(contact=>(
+    <li>{contact.name} - {contact.email} </li>
+))}
 <ul>
     <li>Nouran Hassan - Nouran@gmail.com</li>
     <li>Nouran Hassan - Nouran2@gmail.com</li>
