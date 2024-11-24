4bef6e1a4a700343af3c7780496686c213d5f25e
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-11-24 17:05:53 +0200

    fixing

--- meteor-wallet/imports/ui/App.jsx
+++ meteor-wallet/imports/ui/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ContactForm } from './ContactFrom';
 import { ContactList } from './ContactList';
-
+import {Header} from "./header";
 export const App = () => (
   <div>
+       <Header />
+         <div className="min-h-full">
+         <div className="max-w-4xl mx-auto p-2">
     <h1>Meteor Wallet</h1>
 <ContactForm/>
 <ContactList/>
-
+</div>
+ </div>
   </div>
 );
